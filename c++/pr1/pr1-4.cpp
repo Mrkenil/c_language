bd8e2e854398fcbff0fe1dad497f7503fd250e39
Mrkenil <114163927+Mrkenil@users.noreply.github.com>
2022-12-07 17:17:47 +0530

    Update pr1-4.cpp

--- c++/pr1/pr1-4.cpp
+++ c++/pr1/pr1-4.cpp
@@ -8,7 +8,7 @@ int main(){
 	cout << "Enter character : ";
 	cin >> a;
 	
-	if(a=='a'||a=='U'||a=='O'||a=='I'||a=='E'||a=='a'||a=='u'||a=='o'||a=='i'||a=='e')
+	if(a=='a'||a=='U'||a=='O'||a=='I'||a=='E'||a=='A'||a=='u'||a=='o'||a=='i'||a=='e')
 	{
 		cout << "vowal";
 	}else if((a>=65&&a<=90)||(a>=97&&a<=122)){
